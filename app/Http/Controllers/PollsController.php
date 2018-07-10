3f0e1cdb8239b6d4c217aef49d633ff2edb417a5
Andri Wicaksono <andriwicaksonost@gmail.com>
2018-07-10 12:43:54 +0700

    API Create new record

--- app/Http/Controllers/PollsController.php
+++ app/Http/Controllers/PollsController.php
@@ -14,4 +14,9 @@ class PollsController extends Controller
     public function show($id){
     	return response()->json(Poll::find($id), 200);
     }
+
+    public function store(Request $request){
+    	$poll = Poll::create($request->all());
+    	return response()->json($poll, 201);
+    }
 }
